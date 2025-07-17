0500d9d01ed1fcea17b6dd15b27224849b5a8417
Marcel1853 <74411845+Marcel1853@users.noreply.github.com>
2025-07-17 14:29:36 +0200

    add install

--- install.lua
+++ install.lua
@@ -9,4 +9,10 @@ local base = "https://raw.githubusercontent.com/Marcel1853/FluxNet-Monitor-for-C
 for _, file in ipairs(files) do
     shell.run("wget", base .. file, file)
 end
+
+-- Alle Dateien wurden erfolgreich heruntergeladen
 print("Alle Dateien wurden geladen!")
+print("FluxNet Monitor wurde erfolgreich installiert!")
+print("Reboot in 5 Sekunden...")
+sleep(5) -- Kurze Pause für die Anzeige der Nachricht
+shell.run("reboot") -- Optional: Neustart des Systems nach der Installation
